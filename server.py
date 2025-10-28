a18b4c0b7d839b1fdff779527c5c1799f50b0374
meraeugene <andrew.villalon@1.ustp.edu.ph>
2025-10-28 14:58:10 +0800

    Fix CORS middleware to remove trailing slash from production site URL

--- server.py
+++ server.py
@@ -33,7 +33,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://archivia-official.vercel.app/"],
+    allow_origins=["http://localhost:3000", "https://archivia-official.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
